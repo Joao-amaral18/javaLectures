6a0c4968936b64bd55d706e85ee16232ab456ee6
Joao-amaral18 <joaovitor.lima.amaral@gmail.com>
2022-05-22 15:53:48 -0300

    first commit

--- src/br/com/dio/PrimeiroProgram.java
+++ src/br/com/dio/PrimeiroProgram.java
@@ -6,9 +6,9 @@ public class PrimeiroProgram {
     public static void main(String[] args) {
         Gato gato = new Gato();
 
-//        int a = 5;
-//        int b = 4;
-//        System.out.println("Hello World " + (a+b));
+        int a = 5;
+        int b = 4;
+        System.out.println("Hello World " + (a+b));
 
 
     }
